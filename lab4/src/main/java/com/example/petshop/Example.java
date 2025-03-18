4bc268aa96c4ca4f8e1eb7a84e69aee96ebeedb4
Marina <vlasovamarisha2004@gmail.com>
2025-03-19 00:12:04 +0700

    set many implements for work with liguibase, postgres and tests

---
+++ lab4/src/main/java/com/example/petshop/Example.java
@@ -0,0 +1,7 @@
+
+  //      "id": 1,
+    //        "name": "Jessy",
+      //      "category": { "id": 3, "name": "Cats" },
+        //"tags": [ { "id": 4, "name": "Best" } ],
+        //"status": "unavaible"
+
